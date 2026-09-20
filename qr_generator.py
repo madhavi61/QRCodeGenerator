70ab1d6afb055603952879f6b0acc86c0ad05851
Madhavi Muli <madhavi.manasareddy@gmail.com>
2026-09-20 01:49:37 -0400

    Follow coding best practices and provide comments.

--- qr_generator.py
+++ qr_generator.py
@@ -1,9 +1,12 @@
 import qrcode
 
+# Run the QR code generator
 if __name__ == "__main__":
 
+    # Ask the user to enter the website URL
     url = input("Enter the URL you want to create a QR code for: ")
 
+    # Set up the QR code
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -11,13 +14,17 @@ if __name__ == "__main__":
         border=4
     )
 
+    # Add the URL and generate the QR code
     qr.add_data(url)
     qr.make(fit=True)
 
+    # Create the QR code image
     img = qr.make_image(fill_color="black", back_color="white")
 
+    # Ask the user for a file name
     file_name = input("Enter a name for the QR code file: ")
 
+    # Save the QR code as a PNG file
     img.save(file_name + ".png")
 
     print("QR code was created and saved as " + file_name + ".png")
